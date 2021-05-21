2d83cc1b30f642e47d1af770e3223ae3bc1f886b
Brian Nations <briannations@Brians-MacBook-Pro.local>
2021-05-21 09:58:20 -0700

    added

---
+++ bcrypt-demo.js
@@ -0,0 +1,19 @@
+
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+let original = 'original string';
+let salt = 10;
+
+let hashed;
+
+bcrypt.hash(original, salt)
+  .then(data => {
+    console.log('bcrypt made this:', data);
+  })
+
+// bcrypt.compare(original, '$2b$10$zWPYS8Yl0NcT4nLIt4KjnuiakimHIp.kVH0v/Esv6QOM6Xm/2lwTK')
+// .then(verify => {
+//   console.log('worked?', verify);
+// })
