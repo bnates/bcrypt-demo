7b7101152a881e69b8be01b372ea77c56bfab1e9
Brian Nations <brian@codefellows.com>
2021-05-21 12:57:12 -0700

    Update bcrypt-demo.js

--- bcrypt-demo.js
+++ bcrypt-demo.js
@@ -13,7 +13,7 @@ bcrypt.hash(original, salt)
     console.log('bcrypt made this:', data);
   })
 
-// bcrypt.compare(original, '$2b$10$zWPYS8Yl0NcT4nLIt4KjnuiakimHIp.kVH0v/Esv6QOM6Xm/2lwTK')
-// .then(verify => {
-//   console.log('worked?', verify);
-// })
+bcrypt.compare(original, '$2b$10$zWPYS8Yl0NcT4nLIt4KjnuiakimHIp.kVH0v/Esv6QOM6Xm/2lwTK')
+.then(verify => {
+  console.log('worked?', verify);
+})
